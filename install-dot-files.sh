97b9403378a36a82e573d87195a9042e9e720996
Victor Holanda <victorusu@gmail.com>
2025-04-19 01:00:04 +0200

    Update the install dot files script

--- install-dot-files.sh
+++ install-dot-files.sh
@@ -5,28 +5,63 @@ if [ -d ${HOME}/.dotfiles ]; then
     exit 1
 fi
 
-tools=(zypper dnf apt)
-for t in ${tools[@]}; do
-    mgmt=${t}
-    which ${mgmt} >& /dev/null
-    if [ $? -eq 0 ]; then
-        break
+pkg_manager() {
+    tools=(zypper dnf apt)
+    for t in ${tools[@]}; do
+        which ${t} >& /dev/null
+        if [ $? -eq 0 ]; then
+            break
+        fi
+    done
+    echo $t
+    unset $t
+}
+
+sudo_cmd() {
+    if [ $(id -u) -eq 0 ]; then
+        echo ""
+    else
+        CMD=$(which $1 2> /dev/null)
+        if [ $? -ne 0 ]; then
+            echo "Cannot proceed $1 cmd not found"
+            exit 1
+        fi
+        echo $CMD
+        unset CMD
     fi
-done
+}
 
-echo "using ${mgmt} tool"
-SUDOCMD=$(which sudo)
-if [ $? -ne 0 ]; then
-    echo "Cannot proceed sudo cmd not found"
-    exit 1
-fi
+get_cmd_or_install() {
+    set -e
+    CMD=$(which $1 2> /dev/null)
+    if [ $? -ne 0 ]; then
+        ${SUDOCMD} ${MGMT} install -y $1
+    fi
+    echo $CMD
+    unset CMD
+}
 
-${SUDOCMD} ${mgmt} install -y git stow
+echo "checking package manager... "
+MGMT=$(pkg_manager)
+echo ${MGMT}
+
+echo "checking sudo cmd... "
+SUDOCMD=$(sudo_cmd sudo)
+echo "found ${SUDOCMD}"
+
+echo "checking if git is installed, if not install it... "
+GITCMD=$(get_cmd_or_install git)
+echo "found ${GITCMD}"
+
+echo "checking if stow is installed, if not install it... "
+STOWCMD=$(get_cmd_or_install stow)
+echo "found ${STOWCMD}"
+
 
 cd
-git clone https://github.com/victorusu/dotfiles .dotfiles
+${GITCMD} clone https://github.com/victorusu/dotfiles .dotfiles
 cd .dotfiles
-stow --dotfiles --adopt -t ~/ bash
-git restore bash
-stow --dotfiles -t ~/ vim
+${STOWCMD} --dotfiles --adopt -t ~/ bash
+${GITCMD} restore bash
+${STOWCMD} --dotfiles -t ~/ vim
 exit 0
